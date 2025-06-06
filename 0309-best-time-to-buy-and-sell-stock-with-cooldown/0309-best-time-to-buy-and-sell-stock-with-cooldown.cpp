a385be8549572997d9159b9da91074a2b607ff1a
Pranay Goel <32013103+PranayGoel@users.noreply.github.com>
2025-06-06 14:46:30 +0530

    Time: 2 ms (40.61%), Space: 16.1 MB (50.11%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -23,11 +23,11 @@ public:
 
         for(int ind = n-1; ind >= 0; ind--){
             for(int buy = 0; buy <= 1; buy++){
-                if(buy == 1)
-                    dp[ind][buy] = max( -prices[ind] + dp[ind+1][0], dp[ind+1][1]);
+
+                dp[ind][1] = max( -prices[ind] + dp[ind+1][0], dp[ind+1][1]);
                 
-                else
-                    dp[ind][buy] = max( prices[ind] + dp[ind+2][1], dp[ind+1][0]);
+                
+                dp[ind][0] = max( prices[ind] + dp[ind+2][1], dp[ind+1][0]);
             }
         }
 
