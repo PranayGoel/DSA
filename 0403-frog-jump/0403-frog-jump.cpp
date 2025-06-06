be3a7e7833cfc380f9384f4056cb122d43e4681e
Pranay Goel <32013103+PranayGoel@users.noreply.github.com>
2025-06-06 21:18:21 +0530

    Time: 111 ms (43%), Space: 51.5 MB (41.16%) - LeetHub

---
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+
+    bool f(int ind, int k, vector<int>& stones, int n){
+        
+        if(ind == n-1) return true;
+
+        bool ans = false;
+
+        for(int i = ind+ 1; i < n; i++){
+            if(stones[i] - stones[ind] > k+1){
+                break;
+            }
+            
+            for(int j = -1; j< 2; j++){
+                if((stones[i] - stones[ind]) == k+j){
+                    ans =  f(i, k+j, stones, n) || ans;
+                }
+            }
+
+        }
+        return ans;
+        
+
+    }
+    bool canCross(vector<int>& stones) {
+        if(stones[1] - stones[0] >1 ) return false;
+
+        
+
+        //return f(1, 1, stones, stones.size());
+
+        int n = stones.size();
+
+        unordered_map<int, unordered_set<int>> jumps;
+
+        for(int i = 0; i < n ; i++){
+            jumps[stones[i]] = {};
+        }
+
+        jumps[0].insert(1);
+
+        for(int i = 0; i< n; i++){
+
+            int currStone = stones[i];
+
+
+            for(auto jump : jumps[currStone]){
+                
+                int pos = currStone + jump;
+
+                if(pos == stones[n-1]) return true;
+                if(jumps.find(pos) != jumps.end()){
+                    if(jump-1 > 0) jumps[pos].insert(jump-1);
+                    jumps[pos].insert(jump);
+                    jumps[pos].insert(jump+1);
+                }
+            }
+        }
+        return false;
+
+    }
+};
