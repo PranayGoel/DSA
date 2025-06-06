6170c5b018ec432510997815e45cf47deccd6e8d
Pranay Goel <32013103+PranayGoel@users.noreply.github.com>
2025-06-06 14:34:24 +0530

    Time: 4 ms (73.44%), Space: 14.4 MB (81.78%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -18,21 +18,24 @@ public:
 
         //return f(0, 0, prices, prices.size(),  k, dp);
 
-        vector<vector<int>> dp(prices.size()+1, vector<int>(2*k+1, 0));
+        //vector<vector<int>> dp(prices.size()+1, vector<int>(2*k+1, 0));
+
+        vector<int> ahead(2*k+1,0), cur(2*k+1, 0);
 
         int n = prices.size();
 
         for(int ind = n-1 ; ind >= 0; ind--){
             for(int tran = 2*k-1; tran >= 0; tran--){
                 if(tran%2 == 0){
-                    dp[ind][tran]= max(-prices[ind] +dp[ind+1][tran+1], dp[ind+1][tran]);
+                    cur[tran]= max(-prices[ind] +ahead[tran+1], ahead[tran]);
                 }
                 else{
-                    dp[ind][tran] = max(prices[ind] + dp[ind+1][tran+1], dp[ind+1][tran]);
+                    cur[tran] = max(prices[ind] + ahead[tran+1], ahead[tran]);
                 }
             }
+            ahead = cur;
         }
 
-        return dp[0][0];
+        return ahead[0];
     }
 };
