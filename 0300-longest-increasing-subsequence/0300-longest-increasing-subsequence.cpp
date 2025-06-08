17d78c48d0a640e61842f108735b4304c16f67ce
Pranay Goel <32013103+PranayGoel@users.noreply.github.com>
2025-06-08 23:14:53 +0530

    Time: 4 ms (78.69%), Space: 14 MB (94.43%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -17,48 +17,7 @@ public:
 
     int lengthOfLIS(vector<int>& nums) {    
 
-        //vector<vector<int>>dp(nums.size(), vector<int>(nums.size()+1, -1));
-        //return f(0, -1, nums, nums.size(), dp);
-
-        /* bottom up
-        int n = nums.size();
-        vector<vector<int>>dp(n+1, vector<int>(n+1, 0));
-        // prev_ind is 1 shifted / 1 indexed, because it goes from -1 to n-1, -1 index is not possible so store -1 to n-1 in 0 to n
-        //simply add +1 to prev_ind in dp[ind][prev_ind]
-
-        for(int ind = n-1; ind >=0; ind--){
-            for(int prev_ind = ind-1 ; prev_ind >= -1; prev_ind--){
-                int len = dp[ind+1][prev_ind+1];
-                if(prev_ind == -1 || nums[ind] > nums[prev_ind])
-                    len = max(len, 1 + dp[ind+1][ind+1]);
-
-                dp[ind][prev_ind+1] = len;
-            }
-        }
-
-        return dp[0][-1+1];
-        */
-
-        //bottom up with space optimization
-        // tc - o(n^2), sc - o(n)
-        /*
-        int n = nums.size();
-        vector<int> ahead(n+1, 0), cur(n+1, 0);
-
-        for(int ind = n-1; ind >=0; ind--){
-            for(int prev_ind = ind-1 ; prev_ind >= -1; prev_ind--){
-                int len = ahead[prev_ind+1];
-                if(prev_ind == -1 || nums[ind] > nums[prev_ind])
-                    len = max(len, 1 + ahead[ind+1]);
-
-                cur[prev_ind+1] = len;
-            }
-            ahead= cur;
-        }
-
-        return ahead[-1+1];
-        */
-
+        
         int n=  nums.size();
         vector<int> temp;
         temp.push_back(nums[0]);
