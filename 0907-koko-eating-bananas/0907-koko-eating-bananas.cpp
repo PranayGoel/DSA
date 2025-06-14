9c900d027256f284a39b80b65fd5d0a62372c257
Pranay Goel <32013103+PranayGoel@users.noreply.github.com>
2025-06-14 10:06:14 +0530

    Time: 24 ms (6.31%), Space: 23 MB (41.92%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -0,0 +1,46 @@
+/*
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        
+        int k = 1;
+
+        while(true){
+
+            int count = 0;
+            for(auto pile: piles)
+                count += ceil((double)pile /k);
+
+            if(count <= h) return k;
+            else k++;
+
+        }
+    }
+};
+*/
+
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        
+        int low = 1;
+        int high = *max_element(piles.begin(), piles.end());
+        
+        while(low < high){
+            int mid = low + (high-low)/2;
+            int count = 0;
+            for(auto pile: piles){
+                count += ceil((double)pile/mid);
+            }
+
+            if(count <= h) {
+                high = mid;
+            }
+            else 
+                low = mid+1;
+
+        }
+        return low;
+
+    }
+};
