2a9a916fee7b729b5ec9c3a77de9eac46ad83af7
Pranay Goel <32013103+PranayGoel@users.noreply.github.com>
2025-06-20 08:50:07 +0530

    Time: 31 ms (33.44%), Space: 24.4 MB (17.55%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,44 +1,42 @@
 class Solution {
 public:
-
-    void bfs(int row, int col, vector<vector<int>>& vis, vector<vector<char>>& grid){
+    void bfs(int row, int col, vector<vector<int>> &vis, vector<vector<char>> &grid, int m, int n) {
         vis[row][col] = 1;
-        queue<pair<int,int>> q;
-        q.push(make_pair(row, col));
-        int n= grid.size();
-        int m = grid[0].size();
+        queue<pair<int, int>> q;
+        q.push({row, col});
 
-        while(!q.empty()){
-            int row = q.front().first;
-            int col = q.front().second;
+        int delrow[4] = {-1, 0, 1, 0};
+        int delcol[4] = {0, -1, 0, 1};
+
+        while (!q.empty()) {
+            int r = q.front().first;
+            int c = q.front().second;
             q.pop();
 
-            for(int delrow = -1; delrow <=1; delrow++){
-                for(int delcol = -1; delcol <=1; delcol++){
-                    if(abs(delrow) != abs(delcol)){
-                        int nrow = row + delrow;
-                        int ncol = col + delcol;
-                        if(nrow >=0 && nrow <n && ncol >=0 && ncol <m && !vis[nrow][ncol] && grid[nrow][ncol] == '1'){
-                            vis[nrow][ncol] = 1;
-                            q.push(make_pair(nrow, ncol));
-                        }
-                    }
+            for (int i = 0; i < 4; i++) {
+                int nrow = r + delrow[i];
+                int ncol = c + delcol[i];
+                if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n &&
+                    !vis[nrow][ncol] && grid[nrow][ncol] == '1') {
+                    vis[nrow][ncol] = 1;
+                    q.push({nrow, ncol});
                 }
             }
         }
     }
+
     int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
 
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-
+        vector<vector<int>> vis(m, vector<int>(n, 0));
         int cnt = 0;
-        for(int row = 0; row < n; row++){
-            for(int col = 0; col < m; col++){
-                if(!vis[row][col] && grid[row][col] == '1'){
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!vis[i][j] && grid[i][j] == '1') {
                     cnt++;
-                    bfs(row, col, vis, grid);
+                    bfs(i, j, vis, grid, m, n);
                 }
             }
         }
