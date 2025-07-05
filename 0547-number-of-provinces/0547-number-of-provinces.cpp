1a0e32c670470d1068fb62ed9c4b850de4a91e10
Pranay Goel <32013103+PranayGoel@users.noreply.github.com>
2025-07-05 17:54:05 +0530

    Time: 0 ms (100%), Space: 19.2 MB (92.64%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,38 +1,56 @@
-class Solution {
+class DisjointSet {
 public:
-    void dfs(int node, vector<int> adjList[], vector<int>& vis) {
-        vis[node] = 1;
-        for (auto it : adjList[node]) {
-            if (!vis[it]) {
-                dfs(it, adjList, vis);
-            }
+    vector<int> parent, size;
+
+    DisjointSet(int n){
+        parent.resize(n+1, 0);
+        size.resize(n+1, 0);
+        for(int i = 0; i<= n; i++){
+            parent[i] = i;
+            size[i] = 1;
         }
     }
 
+    int findUPar(int node){
+        if(node== parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionBySize(int u, int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if(ulp_u == ulp_v) return;
+
+        if(size[ulp_u] < size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else{
+            parent[ulp_v] = ulp_u;
+            size[ulp_u]+= size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<int> adjList[n];
-
-        // Convert adjacency matrix to adjacency list
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (isConnected[i][j] == 1 && i != j) {
-                    adjList[i].push_back(j);
-                    adjList[j].push_back(i);
+        DisjointSet ds(n);
+        for(int i = 0; i< n; i++){
+            for(int j = 0; j < n; j++){
+                if(isConnected[i][j]){
+                    ds.unionBySize(i, j);
                 }
             }
         }
 
         int cnt = 0;
-        vector<int> vis(n, 0);
-
-        for (int i = 0; i < n; i++) {
-            if (!vis[i]) {
-                cnt++;
-                dfs(i, adjList, vis);
-            }
+        for(int i = 0; i < n; i++){
+            if(ds.parent[i] == i) cnt++;
         }
-
         return cnt;
     }
 };
