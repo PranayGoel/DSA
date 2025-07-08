f8b469d6ce7e376d176eb8fd3bb6431a1024a22d
Pranay Goel <32013103+PranayGoel@users.noreply.github.com>
2025-07-08 12:41:04 +0530

    Time: 4 ms (24.33%), Space: 52.1 MB (99.02%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,12 +3,11 @@ public:
     bool canJump(vector<int>& nums) {
         int maxIndex = 0;
         for(int i = 0; i< nums.size()-1; i++){
-            if(nums[i] == 0 && maxIndex <= i){
-                break;
-            }
+            if(maxIndex <=i && nums[i] == 0) return false;
             maxIndex = max(nums[i] + i, maxIndex);
+            if(maxIndex >= nums.size()-1) return true;
         }
 
-        return maxIndex>=nums.size()-1 ? true : false;
+        return true;
     }
 };
