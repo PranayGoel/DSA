fd1c96be222f082e7e60230b6f5d2360d226f87a
Pranay Goel <32013103+PranayGoel@users.noreply.github.com>
2025-07-13 01:53:18 +0530

    Time: 0 ms (100%), Space: 34.3 MB (29.79%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -11,55 +11,39 @@
  */
 class Solution {
 public:
-    TreeNode* find_min(TreeNode* root){
-        
-        if(!root->left) return root;
-        
-        return find_min(root->left);
+    TreeNode* findMin(TreeNode* root) {
+        while (root && root->left) {
+            root = root->left;
+        }
+        return root;
     }
 
     TreeNode* deleteNode(TreeNode* root, int key) {
-        
-        if(root == NULL) return NULL;
+        if (!root) return nullptr;
 
-        if(root->val == key){
-            
-            // no child
-            if(!root->left && !root->right){
-                delete root;
-                return NULL;
-            }
-            // 1 child
-
-            else if(root->left && !root->right){
-                TreeNode* temp = root->left;
-                delete root;
-                return temp;
-            }
-            else if(!root->left && root->right){
+        if (key < root->val) {
+            root->left = deleteNode(root->left, key);
+        } 
+        else if (key > root->val) {
+            root->right = deleteNode(root->right, key);
+        } 
+        else {
+            // Node with only one child or no child
+            if (!root->left) {
                 TreeNode* temp = root->right;
                 delete root;
                 return temp;
             }
-
-            // 2 child
-
-            else{
-                TreeNode* minRight = find_min(root->right);
-                root->val= minRight->val;
-                root->right = deleteNode(root->right, minRight->val);
-                return root;
+            if (!root->right) {
+                TreeNode* temp = root->left;
+                delete root;
+                return temp;
             }
 
-
-        }
-        else{
-            if(key < root->val){
-                root->left = deleteNode(root->left, key);
-            }
-            else{
-                root->right = deleteNode(root->right, key);
-            }
+            // Node with two children: Get the inorder successor
+            TreeNode* minRight = findMin(root->right);
+            root->val = minRight->val;
+            root->right = deleteNode(root->right, minRight->val);
         }
 
         return root;
