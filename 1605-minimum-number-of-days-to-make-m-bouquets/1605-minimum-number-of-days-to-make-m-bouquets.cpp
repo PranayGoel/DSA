a94d59df2d5f2d3460122fffea21397f70ade957
Pranay Goel <32013103+PranayGoel@users.noreply.github.com>
2025-07-20 12:16:14 +0530

    Time: 22 ms (61.09%), Space: 70.4 MB (76.48%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,75 +1,45 @@
-/*
 class Solution {
 public:
-    int minDays(vector<int>& bloomDay, int m, int k) {
+    bool isPossible(int days, vector<int>& bloomDay, int m, int k){
+
         int n = bloomDay.size();
+        int flowerCount = 0;
+        int boq = 0;
 
-        if(n < m*k) return -1;
-
-        int min = *min_element(bloomDay.begin(), bloomDay.end());
-        int max = *max_element(bloomDay.begin(), bloomDay.end());
-        
-        if(min == max) return min;
-
-        for(int i = min; i <= max; i++){
-            int count = 0; int b = 0;
-            for(int j = 0; j < n; j++){
-                if(bloomDay[j] <= i) count++;
-                else{
-                    b += count /k;
-                    count = 0;
+        for(int i = 0; i< n; i++){
+            if(bloomDay[i] <= days){
+                flowerCount++;
+                if(flowerCount == k){
+                    boq++;
+                    flowerCount = 0;
                 }
             }
-
-            b+= count/k;
-            if(b == m) return i;
-        }
-        return -1;
-    }
-};
-
-*/
-
-class Solution {
-public:
-    bool possible(vector<int>& bloomDay, int m, int k, int mid){
-        int bouquets = 0;
-        int count = 0;
-
-        for(auto x : bloomDay){
-            if(mid >= x){
-                count++;
-
-                if(count == k){
-                    bouquets++;
-                    count = 0;
-                }
+            else{
+                flowerCount = 0;
             }
-
-            else count = 0;
         }
-
-        if(bouquets >= m) return true;
-
-        return false;
+        return boq >= m;
     }
-
-
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int lo = 1, hi = INT_MIN;
+        if((long long)m*k > bloomDay.size() ) return -1;
+        
+        int low = INT_MAX;
+        int high = INT_MIN;
+
+        for(auto i : bloomDay){
+            low = min(low, i);
+            high = max(high, i);
+        }
+
         int ans = -1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
 
-        for(auto x : bloomDay) hi = max(hi, x);
-
-        while(hi >= lo){
-            int mid = lo + (hi-lo)/2;
-
-            if(possible(bloomDay, m, k, mid)){
+            if(isPossible(mid, bloomDay, m, k)){
                 ans = mid;
-                hi = mid - 1;
+                high = mid-1;
             }
-
-            else lo = mid + 1;
+            else low = mid+1;
         }
 
         return ans;
