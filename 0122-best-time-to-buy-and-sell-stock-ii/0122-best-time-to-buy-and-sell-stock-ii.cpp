ed424752c9433ef562a8e61c002030d385391bdd
Pranay Goel <32013103+PranayGoel@users.noreply.github.com>
2025-07-29 22:15:45 +0530

    Time: 3 ms (42.87%), Space: 19.8 MB (24.03%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,57 +1,30 @@
 class Solution {
 public:
-
-    int f(int ind, int buy, vector<int>& prices, vector<vector<int>>&dp){
-        if(ind == prices.size())
-            return 0;
-
+    int solve(int ind, int buy, vector<int>& prices, vector<vector<int>>& dp){
+        int n = prices.size();
+        if(ind == n) return 0;
         if(dp[ind][buy] != -1) return dp[ind][buy];
-        
         if(buy == 1){
-            return dp[ind][buy] = max( (-prices[ind] + f(ind+1, 0, prices, dp)), f(ind+1, 1, prices, dp));
+            return dp[ind][buy] = max(-prices[ind] + solve(ind+1, 0, prices, dp), solve(ind+1, 1, prices, dp));
         }
         else{
-            return dp[ind][buy] = max( (prices[ind] + f(ind+1, 1, prices, dp)), f(ind+1, 0, prices, dp));
+            return dp[ind][buy] = max(prices[ind] + solve(ind+1, 1, prices, dp), solve(ind+1, 0, prices, dp));
         }
+
+
     }
-
     int maxProfit(vector<int>& prices) {
-        //vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
+        int n = prices.size();
+        vector<vector<int>> dp(n+1, vector<int>(2, 0));
+        //return solve(0, 1, prices, dp);
 
-        //return f(0, 1, prices, dp);
-
-        /*
-        vector<vector<long>> dp(prices.size()+1, vector<long>(2, 0));
-
-        for(int ind = prices.size()-1; ind >=  0; ind--){
-            for(int buy = 0; buy <=1; buy++){
-
-                if(buy == 0){
-                    dp[ind][buy] = max( (+prices[ind] + dp[ind+1][1]), dp[ind+1][0]);
-                }else
-                    dp[ind][buy] = max( (-prices[ind] + dp[ind+1][0]), dp[ind+1][1]);
-            }
-
+        for(int ind = n-1; ind >= 0; ind--){
+            dp[ind][1] = max(-prices[ind] + dp[ind+1][0] , dp[ind+1][1]);
+            dp[ind][0] = max(prices[ind] + dp[ind+1][1], dp[ind+1][0]);
         }
+
         return dp[0][1];
-        */
 
-        vector<long> ahead(2,0), cur(2,0);
-        
-        for(int ind = prices.size()-1; ind>=0; ind--){
-            for(int buy = 0; buy <= 1; buy++){
-                long profit = 0;
-                if(buy){
-                    profit = max((-prices[ind] + ahead[0]), ahead[1]);
 
-                }
-                else
-                    profit = max((prices[ind] + ahead[1]), ahead[0]);
-
-                cur[buy]  =profit;
-            }
-            ahead = cur;
-        }
-        return ahead[1];
     }
 };
