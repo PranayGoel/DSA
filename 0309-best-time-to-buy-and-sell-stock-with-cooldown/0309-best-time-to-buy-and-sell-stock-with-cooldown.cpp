e7c9ab822112e74bf55842c6948b91e817fc58dc
Pranay Goel <32013103+PranayGoel@users.noreply.github.com>
2025-07-29 22:38:18 +0530

    Time: 2 ms (42.34%), Space: 16.3 MB (35.37%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
-/*
-    int f(int ind, int buy, vector<int>& prices, int n, vector<vector<int>>& dp){
+    int solve(int ind, int buy, vector<int>& prices, vector<vector<int>>& dp){
+        int n = prices.size();
         if(ind >= n) return 0;
-
         if(dp[ind][buy] != -1) return dp[ind][buy];
-
         if(buy == 1){
-            return dp[ind][buy] = max( -prices[ind] + f(ind+1, 0, prices, n, dp), f(ind+1, 1, prices, n, dp));
+            return dp[ind][buy] = max(-prices[ind] + solve(ind+1, 0, prices, dp), solve(ind+1, 1, prices, dp));
         }
         else{
-            return dp[ind][buy] = max( prices[ind] + f(ind+2, 1, prices, n, dp), f(ind+1, 0, prices, n, dp));
+            return dp[ind][buy] = max(prices[ind] + solve(ind+2, 1, prices, dp), solve(ind+1, 0, prices, dp));
         }
+
 
     }
-*/
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        //vector<vector<int>> dp(n, vector<int>(2, -1));
-
-        //return f(0, 1, prices, n, dp);
-
-        //vector<vector<int>> dp(n+2, vector<int>(2, 0));
-        vector<int> front1(2, 0), front2(2,0), cur(2,0);
-
+        vector<vector<int>> dp(n+2, vector<int>(2, 0));
+        //return solve(0, 1, prices, dp)
 
         for(int ind = n-1; ind >= 0; ind--){
-            cur[1] = max( -prices[ind] + front1[0], front1[1]);
-            
-            
-            cur[0] = max( prices[ind] + front2[1], front1[0]);
-
-            front2 = front1;
-            front1 = cur;
+            dp[ind][1] = max(-prices[ind] + dp[ind+1][0] , dp[ind+1][1]);
+            dp[ind][0] = max(prices[ind] + dp[ind+2][1], dp[ind+1][0]);
         }
 
-        return cur[1];
+        return dp[0][1];
+
+
     }
 };
