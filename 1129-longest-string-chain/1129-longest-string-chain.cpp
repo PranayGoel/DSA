bf1b3a94ad5c0d2669b2da887c8fd52ebd9f2073
Pranay Goel <32013103+PranayGoel@users.noreply.github.com>
2025-07-30 02:43:39 +0530

    Time: 1890 ms (11.81%), Space: 651.2 MB (6.84%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -1,44 +1,45 @@
 class Solution {
 public:
-    bool compare(string s1, string s2){
-        int n = s1.size();
-        int m = s2.size();
-        if(n != m+1) return false;
+    bool isPossible(string a, string b) {
+        int n = b.size();
+        if(a.size() + 1 != b.size()) return false;
 
-        int i = 0;
-        int j = 0;
+        int i = 0, j  = 0;
+        bool skipped = false;
 
-        while( i < n){
-            if(s1[i] == s2[j]){
+        while(i < a.size() && j < b.size()){
+            if(a[i] == b[j]){
                 i++;
                 j++;
-            }else{
-                i++;
+            }
+            else{
+                if(skipped) return false;
+                skipped = true;
+                j++;
             }
         }
-        if(i == n && j == m) return true;
-        else return false;
-        
+        return true;
     }
-
-    static bool comp(string &s1, string &s2){
-        return s1.size() < s2.size();
+    
+    static bool comp(string a, string b){
+        return a.size() < b.size();
     }
-
     int longestStrChain(vector<string>& words) {
         int n = words.size();
-        sort(words.begin(), words.end(), comp);
         vector<int> dp(n, 1);
-        int maxi =1;
-        for(int i = 1; i< n; i++){
-            for(int j = 0; j < i; j++){
-                if(compare(words[i], words[j]) && dp[i] < dp[j] + 1){
-                    dp[i] = dp[j] + 1;
+        int maxi = 1;
+        sort(words.begin(), words.end(), comp);
+        for(int i = 1; i < n; i++){
+            for(int prev = 0; prev < i; prev++){
+                if(isPossible(words[prev], words[i]) && dp[prev] + 1 > dp[i]){
+                    dp[i] = dp[prev] + 1;
                 }
             }
-            if(dp[i]> maxi) maxi = dp[i];
+            if(dp[i] > maxi){
+                maxi = dp[i];
+            }
         }
-        return maxi;
 
+        return maxi;
     }
 };
